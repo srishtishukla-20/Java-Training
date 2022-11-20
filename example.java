c29521cfddf69d7bb1ca0fba0b8fdd95fc0a5a89
srishtishukla-20 <85153150+srishtishukla-20@users.noreply.github.com>
2022-11-19 21:26:20 -0800

    Add files via upload

---
+++ example.java
@@ -0,0 +1,33 @@
+package stringArray;
+
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums= {4,65,23,89,12};
+		char[] characters= {'&','$','f','4','l'};
+		String[] alphabets= {"String","is","sequence","of","character"};
+		System.out.println(nums[0]);
+		System.out.println(nums[1]);
+		System.out.println(nums[2]);
+		System.out.println(nums[3]);
+		System.out.println(nums[4]);
+		System.out.println(characters[0]);
+		System.out.println(characters[1]);
+		System.out.println(characters[2]);
+		System.out.println(characters[3]);
+		System.out.println(characters[4]);
+		System.out.println(alphabets[0]);
+		System.out.println(alphabets[1]);
+		System.out.println(alphabets[2]);
+		System.out.println(alphabets[3]);
+		System.out.println(alphabets[4]);
+		
+	}
+	
+}
+
+
+
+
