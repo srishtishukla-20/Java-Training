da4ea39a4bb2441a4e50c2405fa421b592fec90d
srishtishukla-20 <85153150+srishtishukla-20@users.noreply.github.com>
2022-11-19 21:25:31 -0800

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,11 @@
+package training;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am Srishti,I am excited to learn Java");
+
+	}
+
+}
