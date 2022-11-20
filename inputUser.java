56d6ffbd8c01693e8fbbac737aa3c7a773299e73
srishtishukla-20 <85153150+srishtishukla-20@users.noreply.github.com>
2022-11-19 21:24:11 -0800

    Add files via upload

---
+++ inputUser.java
@@ -0,0 +1,19 @@
+
+package inputUser;
+import java.util.Scanner;
+
+public class inputUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter your age");
+		Scanner scobject=new Scanner(System.in);
+		int age=scobject.nextInt();
+//		Classname  objectname=new Classname();
+//		newkeyword is used to create object
+		
+		System.out.println("Your age is " +age);
+
+	}
+
+}
