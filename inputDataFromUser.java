d6233ee38c422365e0f251a34dbc6d29d5b090b5
srishtishukla-20 <85153150+srishtishukla-20@users.noreply.github.com>
2022-11-19 21:23:23 -0800

    Add files via upload

---
+++ inputDataFromUser.java
@@ -0,0 +1,37 @@
+package inputData;
+import java.util.Arrays;
+
+public class inputDataFromUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		String name;
+//		int num;
+//		char special;
+//		double decimal;
+//		short small;
+//		long large;
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter one string,one number,one character,one double,one short and one long");
+//		name=sc.next();
+//		num=sc.nextInt();
+//		special=sc.next().charAt(0);
+//		decimal=sc.nextDouble();
+//		small=sc.nextShort();
+//		large=sc.nextLong();
+//		System.out.println(name);
+//		System.out.println(num);
+//		System.out.println(special);
+//		System.out.println(decimal);
+//		System.out.println(small);
+//		System.out.println(large);
+//		
+		int []setvalues= {1,5,7,8,9};
+//		System.out.println(setvalues);
+		System.out.println(Arrays.toString(setvalues));
+		
+		}
+		
+		}
+
+//}
