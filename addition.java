ef26a0ca55f8663cfce4f37497a7cb755c03db38
srishtishukla-20 <85153150+srishtishukla-20@users.noreply.github.com>
2022-11-21 22:02:10 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,19 @@
+package polymorphismSecondExample;
+
+public class addition {
+	int num1;
+	int num2;
+	public int add()//changing the parameters
+	{
+		return num1+num2;
+	}
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	public float add(float a,float b) 
+	{
+		return a+b;
+	}
+
+}
---
+++ methodOverloading.java
@@ -0,0 +1,13 @@
+package polymorphismSecondExample;
+
+public class methodOverloading {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		addition a=new addition();
+		System.out.println(a.add(56.8f,8.7f));
+		System.out.println(a.add(7, 8));
+
+	}
+
+}
