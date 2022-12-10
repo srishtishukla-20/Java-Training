25e6f33fd90ae89f4d4575f9118dce070492ba1c
srishtishukla-20 <85153150+srishtishukla-20@users.noreply.github.com>
2022-12-10 10:49:12 +0530

    Add files via upload
    reverse string in maven

--- AppTest.java
+++ AppTest.java
@@ -1,20 +1,19 @@
-package example.hello.my_first_system;
+package mavenProject.reverse_string;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+  
 	@Test
-	public void testfindMax() {
-		assertEquals(78,App.findMax(new int[] {23,67,78,43} ));
+	public void testfindReverse() {
+		assertEquals("ilajna",App.findReverse(new String("anjali" )));
 		//asserEquals takes 2 parameters, your expected value and the elements	
 	}
-
 }
