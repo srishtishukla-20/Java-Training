da0eb262fade2da50db9fb336c4f89e36c0c266d
srishtishukla-20 <85153150+srishtishukla-20@users.noreply.github.com>
2022-12-10 11:33:37 +0530

    Add files via upload

---
+++ genericsClass.java
@@ -0,0 +1,19 @@
+package genericsTest;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class genericsClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> arrayList= new ArrayList();
+//		arrayList.add("Str1");
+		arrayList.add(54);
+		arrayList.add(645);
+		int a=arrayList.get(1);
+		System.out.println(a);
+
+	}
+
+}
