06bebd96b090fd83f06a43c0b7bf4e2b02bbc366
srishtishukla-20 <85153150+srishtishukla-20@users.noreply.github.com>
2022-12-10 10:47:39 +0530

    Add files via upload
    my first project in maven

--- App.java
+++ App.java
@@ -1,4 +1,4 @@
-package mavenProject.reverse_string;
+package example.hello.my_first_system;
 
 /**
  * Hello world!
@@ -6,13 +6,16 @@ package mavenProject.reverse_string;
  */
 public class App 
 {
-    public static  String findReverse( String word ) {
-    String newName="";
-	for(int i=word.length()-1;i>=0;i--)
-	{
-		char value=word.charAt(i);
-		newName+=value;
-	}
-	return newName;
+    public static int findMax(int arr[])
+    {
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+        	if(max<arr[i]) {
+        		max=arr[i];
+        	}
+        }
+        return max;
     }
-}
+    }
+
--- AppTest.java
+++ AppTest.java
@@ -1,21 +1,20 @@
-package mavenProject.reverse_string;
+package example.hello.my_first_system;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
 	@Test
-	public void testfindReverse() {
-		assertEquals("ilajna",App.findReverse(new String("anjali" )));
+	public void testfindMax() {
+		assertEquals(78,App.findMax(new int[] {23,67,78,43} ));
 		//asserEquals takes 2 parameters, your expected value and the elements	
 	}
+
 }
