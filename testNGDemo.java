f51296cc03dc7abc1e965cdfe53aabfd704485f2
srishtishukla-20 <85153150+srishtishukla-20@users.noreply.github.com>
2022-12-20 12:23:11 +0530

    Add files via upload
    testNG

---
+++ testNGDemo.java
@@ -0,0 +1,12 @@
+package heyNg;
+
+import org.testng.annotations.Test;
+
+public class testNGDemo {
+	@Test
+	public void display() {
+		// TODO Auto-generated method stub
+System.out.println("hi");
+	}
+
+}
